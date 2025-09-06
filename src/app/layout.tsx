5b01ec17b323f7c221f37b6c6432a62e34e365e0
YC815 <yushun@chen.zone>
2025-09-06 21:37:21 +0800

    fix: 更新 ClerkProvider 的 clerkJSUrl 以使用 CDN 連結

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <ClerkProvider 
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      clerkJSUrl="https://js.clerk.com/npm/@clerk/clerk-js@5/dist/clerk.browser.js"
+      clerkJSUrl="https://cdn.jsdelivr.net/npm/@clerk/clerk-js@5/dist/clerk.browser.js"
     >
       <html lang="en" suppressHydrationWarning>
         <head />
